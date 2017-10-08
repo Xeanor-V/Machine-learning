37b45a620ee23ddd7083ec99e63c9f13ea2dd312
DESKTOP-082VF5F\David <vg.ursa@gmail.com>
2017-10-08 20:35:15 +0200

    changed naming

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -3,86 +3,89 @@ import numpy as np, pylab, random, math
 from cvxopt.solvers import qp
 from cvxopt.base import matrix
 
-##The larger the alpha value, the more important the point is to us
-
-##Generate Test Data
-classA = [(random.normalvariate(-1.5, 1),
-           random.normalvariate(0.5, 1),
-           1.0)
-          for i in range(5)] + \
-    [(random.normalvariate(1.5, 1),
-      random.normalvariate(0.5, 1),
-      1.0)
-     for i in range(5)]
-
-classB = [(random.normalvariate(0.0, 0.5),
-           random.normalvariate(-0.5, 0.5),
-           -1.0)
-          for i in range(10)]
-
-data = classA + classB
-random.shuffle(data)
-
-#Kernel Functions
-def linear_kernel(X, Y):
+##The larger the alpha value, the more important the point is to us 
+##from sklearn import svm
+## P matrix, t
+## equation 6
+def Linear_Kernel(X, Y):
     return np.dot(X, Y.T) + 1
 
-def polynomial_kernel(X,Y,p):
-    return linear_kernel(X,Y)**p
+def Build_P(X,t):
+    P_Matrix = [[0 for x in range(len(X))] for y in range(len(X))] 
+    for i in range(len(X)):
+        for j in range(len(X)):
+            P_Matrix[i][j] = t[i]*t[j] * Linear_Kernel(X[i],X[j])
+    return np.array(P_Matrix, dtype = np.dtype('d'))
 
-#Building matrix / vector functions
-def build_P(data):
-    t = [x[2] for x in data]
-    p_matrix = [[0 for x in range(len(data))] for y in range(len(data))]
-    for i in range(len(data)):
-        for j in range(len(data)):
-            p_matrix[i][j] = t[i]*t[j] * linear_kernel(np.array(data[i][0:1]),np.array(data[j][0:1]))
-    return np.array(p_matrix, dtype = np.dtype('d'))
-
-def build_q(N):
+def Build_q(N):
     return np.ones(N) * -1
 
-def build_h(N):
+def Build_h(N):
     return np.zeros(N)
 
-def build_G(N):
-    g_matrix = [[0 for x in range(N)] for y in range(N)]
+def Build_G(N):
+    G_Matrix = [[0 for x in range(N)] for y in range(N)] 
     for i in range(N):
         for j in range(N):
             if(i==j):
-                g_matrix[i][j] = -1
-    return np.array(g_matrix, dtype = np.dtype('d'))
+                G_Matrix[i][j] = -1
+    return np.array(G_Matrix, dtype = np.dtype('d'))
 
-def build_nonzero(alpha_values,data):
-    alpha = []
-    points = []
+def Build_NonZero(Alpha_Values,data):
+    Alpha = []
+    Points = []
     epsilon = 0.00001
-    for i in range(len(alpha_values)):
-        if(math.fabs(alpha_values[i]) > epsilon):
-            points.append(data[i])
-    return np.array(points)
+    for i in range(len(Alpha_Values)):
+        if(math.fabs(Alpha_Values[i]) > epsilon):
+            Points.append(data[i])
+    return np.array(Points)
 
-#tells us which side of the decision boundary the point in on
-def build_indicator(alpha,psoints,T):
-    res = 0.0
-    for i in range(len(alpha)):
-        res += alpha[i]*T[i]*linear_kernel(points,points[i])
+def Indicator(Alpha,points,alpha):
+    res = []     
+    for i in range(len(points)):
+        current = 0.0
+        for j in range(len(data)):
+            res += Alpha[i]*Points[i][2]*Linear_Kernel(Points,Points[i])
     return res
 
-N = len(data)
-P = build_P(data)
-q = build_q(N)
-G = build_G(N)
-h = build_h(N)
+T = np.array([1,-1])
+X = np.array([2,1])
+Y = np.array([3,4])
+N = len(X)
+q = Build_q(N)
+P = Build_P(X,T)
+G = Build_G(N)
+h = Build_h(N)
 
-r = qp(matrix(P) , matrix(q) , matrix(G) , matrix(h))
-alpha = list(r['x'])
+##print(len(Q))
+print(Linear_Kernel(X,Y))
 
-points = build_nonzero(alpha,X,Y)
-indicator = build_indicator(alpha,points,T)
-##print(indicator)
 
-## Plot test data
+r = qp( matrix(P) , matrix(q) , matrix(G) , matrix(h)) 
+alpha = list ( r [ 'x' ])
+
+points = Build_NonZero(alpha,data)
+indicator = Build_Indicator(Alpha,Points,T)
+print(indicator)
+
+##Generate Test Data
+classA = [(random.normalvariate(-1.5, 1), 
+            random.normalvariate(0.5, 1), 
+            1.0)
+            for i in range(5)] + \
+            [(random.normalvariate(1.5, 1), 
+            random.normalvariate(0.5, 1), 
+            1.0)
+            for i in range(5)]
+
+classB = [(random.normalvariate(0.0, 0.5), 
+            random.normalvariate(-0.5, 0.5), 
+            -1.0)
+            for i in range(10)]
+
+data = classA + classB 
+random. shuffle (data)
+
 pylab.hold (True) 
 pylab.plot([p[0] for p in classA], 
             [p[1] for p in classA],
@@ -92,11 +95,12 @@ pylab.plot([p[0] for p in classA],
             'ro')
 pylab.show()
 
-## Plot the decision boundary
+## Plotting the Decision Boundary
+#values go from -4 to 4 with a step of 0.05
 xrange = np.arange(-4, 4, 0.05)
 yrange = np.arange(-4, 4, 0.05)
 
-grid = matrix([[indicator(x,y)
+grid = matrix([[indicator(Points, [x y])
                 for y in yrange]
                for x in xrange])
 
@@ -104,5 +108,3 @@ pylab.contour(xrange, yrange, grid,
               (-1.0, 0.0, 1.0),
               colors=('red', 'black', 'blue'),
               linewidths=(1, 3, 1))
-
-
